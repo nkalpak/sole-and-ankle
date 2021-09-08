7d76bd9038884f6b17385ac0595e91c7438bbe5a
nkalpak <nikitakalpakovski1@outlook.com>
2021-09-08 09:55:02 +0200

    chore: remove all starter files

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
-import GlobalStyles from './components/GlobalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <GlobalStyles />
   </React.StrictMode>,
   document.getElementById('root')
 );
